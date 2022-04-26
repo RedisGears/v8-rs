5f3e04d65b745b4ba4fc88e0d98f81c3781f3daf
meir <meir@redis.com>
2022-04-26 15:29:09 +0300

    improve build.rs

--- build.rs
+++ build.rs
@@ -8,8 +8,24 @@ fn main() {
     println!("cargo:rerun-if-changed=v8_c_api/src/v8_c_api.h");
     println!("cargo:rerun-if-changed=v8_c_api/src/v8_c_api.cpp");
 
-    let res = Command::new("make").args(&["-C", "v8_c_api/"]).output().expect("failed to compile v8_c_api");
-    println!("{}", String::from_utf8(res.stdout).unwrap());
+    if !Command::new("make").args(&["-C", "v8_c_api/"]).status().expect("failed to compile v8_c_api").success() {
+        panic!("failed to compile v8_c_api");
+    }
+
+    let output_dir = env::var("OUT_DIR").expect("Can not find out directory");
+
+    if !Command::new("cp").args(&["v8_c_api/src/libv8.a", &output_dir]).status().expect("failed copy libv8.a to output directory").success() {
+        panic!("failed copy libv8.a to output directory");
+    }
+
+    let v8_monolith_path = match env::var("V8_MONOLITH_PATH") {
+        Ok(path) => path,
+        Err(_) => "v8_c_api/libv8_monolith.a".to_string(),
+    };
+
+    if !Command::new("cp").args(&[&v8_monolith_path, &output_dir]).status().expect("failed copy libv8_monolith.a to output directory").success() {
+        panic!("failed copy libv8_monolith.a to output directory");
+    }
 
     let build = bindgen::Builder::default();
 
@@ -25,5 +41,5 @@ fn main() {
         .write_to_file(out_path.join("v8_c_bindings.rs"))
         .expect("failed to write bindings to file");
 
-    println!("cargo:rustc-flags=-L./v8_c_api/src/ -L./v8_c_api/ -lv8 -lv8_monolith -lstdc++")
+    println!("{}", format!("cargo:rustc-flags=-L{} -lv8 -lv8_monolith -lstdc++", output_dir))
 }
