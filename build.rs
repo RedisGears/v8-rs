54921cd32828b416304e321c4fe472d6b8499de5
Meir Shpilraien (Spielrein) <meir@redis.com>
2023-04-11 12:50:18 +0300

    Added the ability to update the V8 headers files (#14)
    The PR adds the ability to update the V8 headers file and control different build options using evironment variable. The evironment variable are:
    
    * V8_VERSION - will change the default V8 version to use.
    * V8_UPDATE_HEADERS - will update the V8 headers according to the set version, allow to also set the following options:
      * V8_HEADERS_PATH - control where to download the headers zip file, default `v8_c_api/libv8.include.zip`.
      * V8_FORCE_DOWNLOAD_V8_HEADERS - download the V8 headers zip file even if it is already exists.
      * V8_HEADERS_URL - url from where to download the V8 headers zip file.
    * V8_MONOLITH_PATH - control where to download the V8 monolith, default `v8_c_api/libv8_monolith.a`
    * V8_FORCE_DOWNLOAD_V8_MONOLITH - download the V8 monolith even if it is already exists.
    * V8_MONOLITH_URL - url from where to download the V8 monolith file.

--- build.rs
+++ build.rs
@@ -9,74 +9,78 @@ use std::path::Path;
 use std::path::PathBuf;
 use std::process::Command;
 
-fn main() {
-    println!("cargo:rerun-if-changed=v8_c_api/src/v8_c_api.h");
-    println!("cargo:rerun-if-changed=v8_c_api/src/v8_c_api.cpp");
-
-    if !Command::new("make")
-        .args(["-C", "v8_c_api/"])
-        .status()
-        .expect("failed to compile v8_c_api")
-        .success()
-    {
-        panic!("failed to compile v8_c_api");
-    }
-
-    let output_dir = env::var("OUT_DIR").expect("Can not find out directory");
-
-    if !Command::new("cp")
-        .args(["v8_c_api/src/libv8.a", &output_dir])
-        .status()
-        .expect("failed copy libv8.a to output directory")
-        .success()
-    {
-        panic!("failed copy libv8.a to output directory");
-    }
-
-    let v8_monolith_path = match env::var("V8_MONOLITH_PATH") {
-        Ok(path) => path,
-        Err(_) => "v8_c_api/libv8_monolith.a".to_string(),
-    };
-
-    let version = "10.8.168.21";
-
-    let arch = match std::env::consts::ARCH {
+lazy_static::lazy_static! {
+    static ref ARCH: &'static str = match std::env::consts::ARCH {
         "x86_64" => "x64",
         "aarch64" => "arm64",
         _ => panic!("Given arch are not support: {}", std::env::consts::ARCH),
     };
 
-    let os = match std::env::consts::OS {
+    static ref  OS: &'static str = match std::env::consts::OS {
         "linux" => "linux",
         "macos" => "apple-darwin",
         _ => panic!("Os '{}' are not supported", std::env::consts::OS),
     };
 
-    let v8_monolith_url = match env::var("V8_MONOLITH_URL") {
-        Ok(path) => path,
-        Err(_) => format!("http://redismodules.s3.amazonaws.com/redisgears/dependencies/libv8_monolith.{}.{}.{}.a", version, arch, os),
-    };
+    static ref V8_VERSION: String = env::var("V8_VERSION").unwrap_or("10.8.168.21".into());
+    static ref V8_HEADERS_PATH: String = env::var("V8_HEADERS_PATH").unwrap_or("v8_c_api/libv8.include.zip".into());
+    static ref V8_HEADERS_URL: String = env::var("V8_HEADERS_URL").unwrap_or(format!("http://redismodules.s3.amazonaws.com/redisgears/dependencies/libv8.{}.include.zip", *V8_VERSION));
+    static ref V8_MONOLITH_PATH: String = env::var("V8_MONOLITH_PATH").unwrap_or("v8_c_api/libv8_monolith.a".into());
+    static ref V8_MONOLITH_URL: String = env::var("V8_MONOLITH_URL").unwrap_or(format!("http://redismodules.s3.amazonaws.com/redisgears/dependencies/libv8_monolith.{}.{}.{}.a", *V8_VERSION, *ARCH, *OS));
 
-    if !Path::new(&v8_monolith_path).exists() {
-        // download libv8_monolith.a
-        if !Command::new("wget")
-            .args(["-O", &v8_monolith_path, &v8_monolith_url])
-            .status()
-            .expect("failed downloading libv8_monolith.a")
-            .success()
-        {
-            panic!("failed downloading libv8_monolith.a");
-        }
-    }
+    static ref V8_HEADERS_DIRECTORY: &'static str = "v8_c_api/src/v8include/";
+    static ref LIBV8_PATH: &'static str = "v8_c_api/src/libv8.a";
 
-    if !Command::new("cp")
-        .args([&v8_monolith_path, &output_dir])
+    static ref V8_UPDATE_HEADERS: bool = env::var("V8_UPDATE_HEADERS").map(|v| v == "yes").unwrap_or(false);
+    static ref V8_FORCE_HEADERS_DOWNLOAD: bool = env::var("V8_FORCE_DOWNLOAD_V8_HEADERS").map(|v| v == "yes").unwrap_or(false);
+    static ref V8_FORCE_MONOLITH_DOWNLOAD: bool = env::var("V8_FORCE_DOWNLOAD_V8_MONOLITH").map(|v| v == "yes").unwrap_or(false);
+}
+
+fn run_cmd(cmd: &str, args: &[&str]) {
+    let failure_message = format!("Failed running command: {} {}", cmd, args.join(" "));
+    if !Command::new(cmd)
+        .args(args)
         .status()
-        .expect("failed copy libv8_monolith.a to output directory")
+        .expect(&failure_message)
         .success()
     {
-        panic!("failed copy libv8_monolith.a to output directory");
+        panic!("{}", failure_message);
     }
+}
+
+fn main() {
+    println!("cargo:rerun-if-changed=v8_c_api/src/v8_c_api.h");
+    println!("cargo:rerun-if-changed=v8_c_api/src/v8_c_api.cpp");
+
+    if *V8_UPDATE_HEADERS {
+        // download and update headers
+        if *V8_FORCE_HEADERS_DOWNLOAD {
+            run_cmd("rm", &["-rf", &V8_HEADERS_PATH]);
+        }
+        if !Path::new(V8_HEADERS_PATH.as_str()).exists() {
+            run_cmd("wget", &["-O", &V8_HEADERS_PATH, &V8_HEADERS_URL]);
+        }
+
+        run_cmd("rm", &["-rf", *V8_HEADERS_DIRECTORY]);
+        run_cmd("unzip", &[&V8_HEADERS_PATH, "-d", *V8_HEADERS_DIRECTORY]);
+    }
+
+    run_cmd("make", &["-C", "v8_c_api/"]);
+
+    let output_dir = env::var("OUT_DIR").expect("Can not find out directory");
+
+    run_cmd("cp", &[*LIBV8_PATH, &output_dir]);
+
+    if *V8_FORCE_MONOLITH_DOWNLOAD {
+        run_cmd("rm", &["-rf", &V8_MONOLITH_PATH]);
+    }
+
+    if !Path::new(V8_MONOLITH_PATH.as_str()).exists() {
+        // download libv8_monolith.a
+        run_cmd("wget", &["-O", &V8_MONOLITH_PATH, &V8_MONOLITH_URL]);
+    }
+
+    run_cmd("cp", &[&V8_MONOLITH_PATH, &output_dir]);
 
     let build = bindgen::Builder::default();
 
