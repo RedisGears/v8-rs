f53c5768343a49cc80ad5360aff9ad00d7917214
Meir Shpilraien (Spielrein) <meir@redis.com>
2023-07-22 13:39:21 +0300

    Seperate patform initialisation from V8 initialisation. (#45)
    Seperate patform initialisation from V8 initialisation.
    
    The platform initialisation must happened on the process main thread,
    otherwise it causes crashes if later, trying to run JS code on the main thread.
    
    Initialising platform is cheep and can happened on processes start. Initialise the V8
    can be heavy and might requere to happened only when V8 is in fact in use.
    
    The PR Splits the platform initialisation from the V8 initialisation so the user can initialise
    them separately.

--- src/v8/mod.rs
+++ src/v8/mod.rs
@@ -4,7 +4,7 @@
  * the Server Side Public License v1 (SSPLv1).
  */
 
-use crate::v8_c_raw::bindings::{v8_Dispose, v8_Initialize, v8_Version};
+use crate::v8_c_raw::bindings::{v8_Dispose, v8_Initialize, v8_InitializePlatform, v8_Version};
 
 use std::ffi::{CStr, CString};
 use std::ptr;
@@ -42,16 +42,24 @@ pub trait OptionalTryFrom<T>: Sized {
     fn optional_try_from(value: T) -> Result<Option<Self>, Self::Error>;
 }
 
-/// Initialize the v8, must be called before any other v8 API.
-pub fn v8_init(thread_pool_size: i32, flags: Option<&str>) -> Result<(), &'static str> {
+/// Initialize default platform, must be called on the process main thread before calling any other v8 API (including [`v8_init`]).
+pub fn v8_init_platform(thread_pool_size: i32, flags: Option<&str>) -> Result<(), &'static str> {
     let flags_cstr = flags.map(|v| CString::new(v).unwrap());
     let res = unsafe {
-        v8_Initialize(
-            ptr::null_mut(),
+        v8_InitializePlatform(
             thread_pool_size,
             flags_cstr.as_ref().map_or(ptr::null_mut(), |v| v.as_ptr()),
         )
     };
+    match res {
+        1 => Ok(()),
+        _ => Err("The V8 default platform failed to initialise."),
+    }
+}
+
+/// Initialize the v8, must be called before any other v8 API.
+pub fn v8_init() -> Result<(), &'static str> {
+    let res = unsafe { v8_Initialize(ptr::null_mut()) };
     match res {
         1 => Ok(()),
         _ => Err("The V8 Engine failed to initialise."),
@@ -63,10 +71,8 @@ pub fn v8_init(thread_pool_size: i32, flags: Option<&str>) -> Result<(), &'stati
 pub fn v8_init_with_error_handlers(
     fatal_error_handler: Box<FatalErrorCallback>,
     oom_error_handler: Box<OutOfMemoryErrorCallback>,
-    thread_pool_size: i32,
-    flags: Option<&str>,
 ) -> Result<(), &'static str> {
-    v8_init(thread_pool_size, flags)?;
+    v8_init()?;
 
     unsafe {
         FATAL_ERROR_CALLBACK = Some(fatal_error_handler);
